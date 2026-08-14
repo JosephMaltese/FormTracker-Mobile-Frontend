13974444bf1adbfc55cdc7daee8e84c39143c4ea
JosephMaltese <josephmaltese34@gmail.com>
2026-08-13 21:13:01 -0400

    tab icons

--- src/app/(authorized)/_layout.tsx
+++ src/app/(authorized)/_layout.tsx
@@ -2,6 +2,7 @@ import { Tabs } from 'expo-router';
 import {ReactNode} from "react";
 import {appBackgroundColorHexDark, appBackgroundColorHexLight} from "@/lib/constants";
 import {useColorScheme} from "react-native";
+import Octicons from '@expo/vector-icons/Octicons';
 
 export default function AuthorizedLayout(): ReactNode {
     const isDark = useColorScheme() === "dark";
@@ -25,21 +26,29 @@ export default function AuthorizedLayout(): ReactNode {
                 name="index"
                 options={{
                     title: "Home",
+                    tabBarIcon: ({ color, focused }) => (
+                        <Octicons name={"home"} size={24} color={color} />
+                    ),
                 }}
             />
             <Tabs.Screen
                 name="statistics"
                 options={{
                     title: "statistics",
+                    tabBarIcon: ({ color, focused }) => (
+                        <Octicons name={"graph"} size={24} color={color} />
+                    ),
                 }}
             />
             <Tabs.Screen
                 name="profile"
                 options={{
                     title: "profile",
+                    tabBarIcon: ({ color, focused }) => (
+                        <Octicons name={"person"} size={24} color={color} />
+                    ),
                 }}
             />
-
         </Tabs>
     );
 }
